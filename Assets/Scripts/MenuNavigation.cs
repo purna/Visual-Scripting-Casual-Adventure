68a8388e215b7f03a45ef13677d9e3c2de59f527
Nigel Morris <nigel@pixelagent.co.uk>
2024-02-08 22:26:53 +0000

    8feb

---
+++ Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System;
+
+
+public class MenuNavigation : MonoBehaviour
+    {
+        public Selectable DefaultSelection;
+        public bool ForceSelection = false;
+
+        void Start()
+        {
+        Cursor.visible = true;
+        Screen.lockCursor = false;
+        EventSystem.current.SetSelectedGameObject(null);
+        }
+
+        void LateUpdate()
+        {
+            if (EventSystem.current.currentSelectedGameObject == null || ForceSelection)
+            {
+                EventSystem.current.SetSelectedGameObject(DefaultSelection.gameObject);
+            }
+        }
+
+        void OnDisable()
+        {
+            if (ForceSelection && EventSystem.current.currentSelectedGameObject == DefaultSelection.gameObject)
+            {
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+        }
+    }
+
